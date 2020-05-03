ce6ffb6b8383483a81435c7d32a78c6c2de16e5a
kanade0525 <a16111k@reitaku.jp>
2020-05-03 13:39:13 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>hello React!</div>
-  );
+const App = () => (<Counter/>);
+
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    this.state = {count: 0}
+  }
+
+  countUp = () => {
+    this.setState({count: this.state.count +1 })
+  }
+
+  countDown = () => {
+    this.setState({count: this.state.count -1 })
+  }
+
+  render(){
+
+    return(
+      <>
+        <p>count: {this.state.count}</p>
+        <button onClick={this.countUp}>+1</button>
+        <button onClick={this.countDown}>-1</button>
+      </>
+    )
+  }
+
 }
 
 export default App;
