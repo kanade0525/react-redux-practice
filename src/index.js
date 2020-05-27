8c71d8a84f674e5397febc3ed5f35c7a228185d2
kanadeishida0527 <hiryuutou.kaito@gmail.com>
2020-05-27 22:29:18 +0900

    apply material-ui to eventsIndex

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import reducer from './reducers';
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
 import EventsShow from './components/events_show';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Route, Switch} from 'react-router-dom'
@@ -18,16 +19,18 @@ const enhancer = process.env.NODE_ENV === 'development' ?
 const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <Route path='/events/new' component={EventsNew} />
-        <Route path='/events/:id' component={EventsShow} />
-        <Route exact path='/' component={EventsIndex} />
-        <Route exact path='/events' component={EventsIndex} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <MuiThemeProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/events/new' component={EventsNew} />
+          <Route path='/events/:id' component={EventsShow} />
+          <Route exact path='/' component={EventsIndex} />
+          <Route exact path='/events' component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
